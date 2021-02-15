eea4e5254dc942d65c2f747196e49cd9d078c8b7
Kelly <kelly.compton@bryanuniversity.edu>
2021-02-14 23:57:46 -0500

    #

---
+++ week3/joinsplit/joinsplit.py
@@ -0,0 +1,5 @@
+csv = "Eric John Michael Terry Graham Terry Brian"
+friends_list = csv.split()
+friends_list2 = "-".join(friends_list)
+print(friends_list)
+print(friends_list2)
---
+++ week3/tuple/tuple.py
@@ -0,0 +1,5 @@
+person = ('robert', '1982', 'rocky', 'supervisor', 'arizona')
+print(person[0])
+
+numbers = (1,2,3,4,5,6,7)
+print(numbers[1:4])
